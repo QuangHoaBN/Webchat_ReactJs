22e5b4e7b77f25b0b021d2aeff03919151b372e6
Quang Hoa <hoaqn.1811@gmail.com>
2019-09-19 18:11:33 +0700

    Fix Merge Branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login_SignUp from './views/author/author.js';
 import Homepage from "./views/main/Homepage.js";
