62ec224faa06c3612cbd2fa85a4af33595ba3442
Quang Hoa <hoaqn.1811@gmail.com>
2019-09-23 01:41:33 +0700

    Create Content Send

--- src/views/main/content/form_chat.js
+++ src/views/main/content/form_chat.js
@@ -3,23 +3,48 @@ import './form_chat.css';
 import Person from './images/person.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import Inbox_Send from './inbox_send';
 
 class Form_Chat extends Component {
+    constructor(props) {
+        super(props);
+        let now = new Date();
+        this.state= {
+            in_text: '',
+            out_text: '',
+            currenNow: now.getHours()+':'+now.getMinutes(),
+            isSended: false,
+            status: ''
+        };
+        this.input_text=this.input_text.bind(this);
+        this.send=this.send.bind(this);
+    }
+    input_text(text){
+        this.setState({in_text: text.target.value});
+    }
+    send(){
+        let now =new Date();
+        this.setState({out_text: this.state.in_text,currenNow: now.getHours()+':'+now.getMinutes(), status: 'Đã gửi',isSended: true ,in_text: ''});
+    
+    }
     render() {
         return (
             <div className='form-chat container-fluid'>
                 <div className='header row'>
                     <div>
+                        {/*eslint-disable-next-line jsx-a11y/img-redundant-alt */}
                         <img width='32px' height='32px' src={Person} alt='error image'></img>
                     </div>
                     <div className='name'>
                         <p>Nguyễn Quang Hòa</p>
                     </div>
                 </div>
-                <div className='content row'></div>
+                <div className='content row'>
+                    {!this.state.isSended || this.state.out_text==='' ? '' : <Inbox_Send content={this.state.out_text} date={this.state.currenNow} status={this.state.status}/>}
+                </div>
                 <div className='footer row'>
-                    <input className='col-xl-10 chat_input' value='' onChange='' placeholder='Nhập ...'></input>
-                    <div className='col-xl-2 icon-sent'>
+                    <input className='col-xl-11 chat_input' value={this.state.in_text} onChange={this.input_text} placeholder='Nhập ...'></input>
+                    <div className='col-xl-1 icon-sent' onClick={this.send}>
                         <FontAwesomeIcon icon={faPaperPlane} />
                     </div>
                 </div>
